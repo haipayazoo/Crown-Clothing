6be78f3fe8fe829e0b1ffd69f6a2ac57f521099b
Cameron Mathos <haipayazoo@gmail.com>
2020-05-13 15:35:21 -0700

    Moving over front-end code to client folder

--- src/components/custom-button/custom-button.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { CustomButtonContainer } from './custon-button.styles';
-
-const CustomButton = ({ children, ...props }) => (
-  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
-);
-
-export default CustomButton;
