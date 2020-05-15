8ff5709b0af536c120452ffba9e575366544baaf
Cameron Mathos <haipayazoo@gmail.com>
2020-05-15 12:41:18 -0700

    Implemented POST function for Stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,45 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_buPpyyqUXN8Dhg1pPOxrPKNY00i1UZO7LT';
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_buPpyyqUXN8Dhg1pPOxrPKNY00i1UZO7LT';
 
-  const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
-  };
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        })
+            .then(response => {
+                alert('Payment successful');
+            })
+            .catch(error => {
+                console.log('Payment error', JSON.parse(error));
+                alert(
+                    'There was an issue with your payment. Please be sure you use the provided credit card.'
+                );
+            });
+    };
 
-  return (
-    <StripeCheckout
-      label="Pay Now"
-      name="CRWN Clothing Ltd."
-      billingAddress
-      shippingAddress
-      image="https://svgshare.com/i/CUz.svg"
-      description={`Your total is $${price}`}
-      amount={priceForStripe}
-      panelLabel="Pay Now"
-      token={onToken}
-      stripeKey={publishableKey}
-    />
-  );
+    return (
+        <StripeCheckout
+            label="Pay Now"
+            name="CRWN Clothing Ltd."
+            billingAddress
+            shippingAddress
+            image="https://svgshare.com/i/CUz.svg"
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
 };
 
 export default StripeCheckoutButton;
--- client/src/redux/user/user.types.js
+++ client/src/redux/user/user.types.js
@@ -12,3 +12,5 @@ const UserActionTypes = {
     SIGN_UP_SUCCESS: 'SIGN_UP_SUCCESS',
     SIGN_UP_FAILURE: 'SIGN_UP_FAILURE'
 };
+
+export default UserActionTypes;
